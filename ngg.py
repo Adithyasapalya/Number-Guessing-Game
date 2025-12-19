06cae387bbb2b814925a03abb9a63b69824110f4
Adithya Sapalya <adithyasapalya30@gmail.com>
2025-12-19 10:02:47 +0530

    ranges added

--- ngg.py
+++ ngg.py
@@ -1,4 +1,15 @@
 import random
 
-r= random.randrange(-5,11)
-print(r)
+top_0f_range=input("type a number :")
+if top_0f_range.isdigit():
+    top_0f_range=int(top_0f_range)
+
+    if top_0f_range <=0:
+        print("Please type a number larger than 0 next time.")
+        exit()
+else:
+    print("Please type a number next time.")
+    exit()
+
+
+r_n= random.randint(-5,11)
